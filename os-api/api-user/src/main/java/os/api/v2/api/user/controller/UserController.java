dcd98c83e43d9c47c5aedee5c065297d84dbdd62
qomolang <1065953740@qq.com>
2022-08-02 23:12:00 +0800

    2022-08-02 23:12
    1、新增模块代码

--- os-api/api-user/src/main/java/os/api/v2/api/user/controller/UserController.java
+++ os-api/api-user/src/main/java/os/api/v2/api/user/controller/UserController.java
@@ -9,10 +9,17 @@
 // +----------------------------------------------------------------------
 package os.api.v2.api.user.controller;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import os.api.v2.common.base.common.Result;
+import os.api.v2.common.base.utils.jwt.JwtUtils;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * os.api.v2.api.user.controller
@@ -28,5 +35,15 @@ public class UserController {
     @RequestMapping(value = "/aaa", method = RequestMethod.GET)
     public Result<String> aaa() {
         return new Result<>(Result.SUCCESS, "666");
+    }
+
+    @RequestMapping(value = "/module", method = RequestMethod.POST)
+    public Result<List<Map<String, Object>>> module() {
+        Subject subject = SecurityUtils.getSubject();
+        PrincipalCollection principal = subject.getPrincipals();
+        System.out.println(principal);
+        Long userId = JwtUtils.getUserId((String) principal.getPrimaryPrincipal());
+        System.out.println(userId);
+        return null;
     }
 }
