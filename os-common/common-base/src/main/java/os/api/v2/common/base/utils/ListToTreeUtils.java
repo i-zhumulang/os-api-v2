4ecaa6dfbe902f3eee079c66f28a0e95289376ed
qomolang <1065953740@qq.com>
2022-08-28 23:58:38 +0800

    2022-08-28 23:58
    1、新增ListToTreeUtils

--- os-common/common-base/src/main/java/os/api/v2/common/base/utils/ListToTreeUtils.java
+++ os-common/common-base/src/main/java/os/api/v2/common/base/utils/ListToTreeUtils.java
@@ -19,7 +19,7 @@ import java.util.List;
  * @version 2.0.0
  * @date 2022-08-28 23:52
  */
-public class ListToTree<T> {
+public class ListToTreeUtils<T> {
 
     // 流程：数据库查出的菜单记录装载在承载菜单的列表中---
     //         构建树（获取根节点，遍历根节点，对每一个根节点构建子树）---得到最终树形菜单
@@ -28,7 +28,7 @@ public class ListToTree<T> {
     private List<T> menuList = new ArrayList<>();
 
     // 带参构造器，将数据库中的菜单数据记录，装载在我们承载菜单的列表中
-    public ListToTree(List<T> menuList) {
+    public ListToTreeUtils(List<T> menuList) {
         this.menuList = menuList;
     }
 
