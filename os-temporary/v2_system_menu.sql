64bf859615fb3bb5bcc681e5842bb5fd0841f033
qomolang <1065953740@qq.com>
2022-09-01 22:50:49 +0800

    2022-09-01 22:50
    1、数据库备份

---
+++ os-temporary/v2_system_menu.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: v2_system
+-- ------------------------------------------------------
+-- Server version	5.7.38
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `menu`
+--
+
+DROP TABLE IF EXISTS `menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `menu` (
+  `id` bigint(20) unsigned NOT NULL,
+  `parent_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '上级菜单ID',
+  `module_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '所属模块',
+  `name_en` varchar(30) NOT NULL DEFAULT '' COMMENT '英文名称',
+  `name_zh` varchar(10) NOT NULL DEFAULT '' COMMENT '中文名称',
+  `uri` varchar(50) NOT NULL DEFAULT '' COMMENT '地址',
+  `sorting` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '排序',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='模块-菜单';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu`
+--
+
+LOCK TABLES `menu` WRITE;
+/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+INSERT INTO `menu` VALUES (1100177340668952576,0,1100176417150205952,'system.module','模块菜单','#',0),(1100177341767860224,1100177340668952576,1100176417150205952,'system.module.index','模块列表','/system/module/index',0),(1100177342397005824,1100177340668952576,1100176417150205952,'system.menu.index','菜单列表','/system/menu/index',1),(1102868720906162176,1100177340668952576,1100176417150205952,'system.operate.index','菜单操作','/system/operate/index',2),(1102871794815094784,0,1100176417951318016,'user.user','会员管理','#',2),(1102872191327817728,1102871794815094784,1100176417951318016,'user.user.index','会员列表','/user/user/index',0),(1102872407070232576,1102871794815094784,1100176417951318016,'user.role.index','角色列表','/user/role/index',1),(1102873120315822080,1102871794815094784,1100176417951318016,'user.rolemodule.index','角色模块','/user/rolemodule/index',2),(1102873569563529216,1102871794815094784,1100176417951318016,'user.rolemenu.index','角色菜单','/user/rolemenu/index',3),(1102873731409137664,1102871794815094784,1100176417951318016,'user.roleoperate.index','角色操作','/user/roleoperate/index',4);
+/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-01 22:48:53
---
+++ os-temporary/v2_user_user_account.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: v2_user
+-- ------------------------------------------------------
+-- Server version	5.7.38
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user_account`
+--
+
+DROP TABLE IF EXISTS `user_account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_account` (
+  `user_id` bigint(20) unsigned NOT NULL COMMENT '用户ID',
+  `mobile` varchar(11) NOT NULL DEFAULT '' COMMENT '手机号码',
+  `authentication` varchar(255) NOT NULL DEFAULT '' COMMENT '密码',
+  `salt` char(32) NOT NULL DEFAULT '' COMMENT '加密用的盐',
+  `iv` char(16) NOT NULL DEFAULT '' COMMENT '一个非空初始化向量',
+  `state` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '状态',
+  `deleted_state` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '是否删除(0:否,1:是)',
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='用户-账号';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_account`
+--
+
+LOCK TABLES `user_account` WRITE;
+/*!40000 ALTER TABLE `user_account` DISABLE KEYS */;
+INSERT INTO `user_account` VALUES (1035433978183557120,'18120827456','IpIj+iIo3C7GeURjG30EnA==','CeKASEI8J8IZJa67F2PkEn4X9mDihzpe','uoI20c88igFvi6eE',0,0),(1103264021471051776,'18120827455','KcQiaEM5yPPhIZZ0KEC/4A==','7BW5yoPsjIolAmvwCqKC5HG4TXSquozQ','xcqFLzqLLXU8PyPy',0,0);
+/*!40000 ALTER TABLE `user_account` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-01 22:49:33
