f4dd9196dfa96c336b1aa6436e71742903a9f194
linbaby <1065953740@qq.com>
2022-09-02 17:07:02 +0800

    2022-09-02 17:07
    1、操作获取权限
    2、数据库调整

--- os-api/api-system/src/main/java/os/api/v2/api/system/service/operate/impl/OperateOptionsServiceImpl.java
+++ os-api/api-system/src/main/java/os/api/v2/api/system/service/operate/impl/OperateOptionsServiceImpl.java
@@ -9,11 +9,19 @@
 // +----------------------------------------------------------------------
 package os.api.v2.api.system.service.operate.impl;
 
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.stereotype.Service;
 import os.api.v2.api.system.service.operate.IOperateOptionsService;
 import os.api.v2.common.base.common.Result;
+import os.api.v2.model.service.system.dto.menuoperate.MenuOperateModelDto;
+import os.api.v2.model.service.system.dto.module.ModuleModelDto;
+import os.api.v2.model.service.system.service.menuoperate.IGetListByIdListService;
+import os.api.v2.model.service.system.service.module.IModuleService;
+import os.api.v2.model.service.system.vo.module.ModuleModelVo;
+import os.api.v2.service.service.user.service.menuoperate.IMenuOperateService;
+import os.api.v2.service.service.user.vo.menuoperate.MenuOperateServiceVo;
 
-import java.util.Map;
+import java.util.*;
 
 /**
  * os.api.v2.api.system.service.operate.impl.OperateOptionsServiceImpl
@@ -24,8 +32,92 @@ import java.util.Map;
  */
 @Service
 public class OperateOptionsServiceImpl implements IOperateOptionsService {
+    @DubboReference(version = "2.0.0")
+    protected IModuleService iModuleService;
+
+    @DubboReference(version = "2.0.0")
+    protected IMenuOperateService iMenuOperateService;
+
+    @DubboReference(version = "2.0.0")
+    protected IGetListByIdListService iGetListByIdListService;
+
     @Override
     public Result<Map<String, Object>> options() {
-        return null;
+        // 获取数据操作权限ID
+        List<Long> menuOperateIdList = getMenuOperateIdList();
+        // 获取数据操作权限数据
+        List<Map<String, Object>> menuOperateList = getMenuOperateList(menuOperateIdList);
+        Map<String, Object> map = new HashMap<>();
+        map.put("operate", menuOperateList);
+        List<ModuleModelDto> moduleModelDtoList = getModuleModelDtoList();
+        map.put("module", moduleModelDtoList);
+        return new Result<>(Result.SUCCESS, map);
+    }
+    /**
+     * 获取数据操作权限ID
+     *
+     * @return List<Long>
+     * @author 吴荣超
+     * @date 22:25 2022/8/29
+     */
+    private List<Long> getMenuOperateIdList() {
+        MenuOperateServiceVo menuOperateServiceVo = new MenuOperateServiceVo();
+        menuOperateServiceVo.setRoleId(1);
+        menuOperateServiceVo.setSystemModuleId(1100176417150205952L);
+        menuOperateServiceVo.setSystemMenuId(1100176417150205952L);
+        Result<List<Long>> result = iMenuOperateService.getSystemMenuOperateIdList(menuOperateServiceVo);
+        if (Objects.equals(result.getFlag(), Result.FAILURE)) {
+            return new ArrayList<>();
+        }
+        return result.getData();
+    }
+
+    /**
+     * getMenuOperateList
+     *
+     * @param menuOperateIdList
+     * @return List<Map<String,Object>>
+     * @author 吴荣超
+     * @date 22:25 2022/8/29
+     */
+    private List<Map<String, Object>> getMenuOperateList(List<Long> menuOperateIdList) {
+        String[] fieldArray = {
+                "location",
+                "name_en",
+                "name_zh",
+                "type",
+        };
+        Result<List<MenuOperateModelDto>> result = iGetListByIdListService.getListByIdList(menuOperateIdList, fieldArray);
+        List<Map<String, Object>> mapList = new ArrayList<>();
+        if (Objects.equals(result.getFlag(), Result.FAILURE)) {
+            return mapList;
+        }
+        for (MenuOperateModelDto menuOperateModelDto : result.getData()) {
+            if ("TABLE-HEAD".equals(menuOperateModelDto.getLocation())) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("nameEn", menuOperateModelDto.getNameEn());
+                map.put("nameZh", menuOperateModelDto.getNameZh());
+                map.put("type", menuOperateModelDto.getType());
+                mapList.add(map);
+            }
+        }
+        return mapList;
+    }
+
+    /**
+     * getModuleModelDtoList
+     *
+     * @return List<ModuleModelDto>
+     * @author 吴荣超
+     * @date 22:28 2022/8/29
+     */
+    private List<ModuleModelDto> getModuleModelDtoList() {
+        ModuleModelVo moduleModelVo = new ModuleModelVo();
+        moduleModelVo.setFieldArray(new String[]{
+                "id",
+                "name_zh"
+        });
+        Result<List<ModuleModelDto>> result = iModuleService.getModuleList(moduleModelVo);
+        return result.getData();
     }
 }
