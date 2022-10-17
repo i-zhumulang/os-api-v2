5f4cfe5926072ead23f8a5b4b85a22e4622f39a7
qomolang <1065953740@qq.com>
2022-10-17 22:42:49 +0800

    2022-10-17 22:42
    1、表名更改

---
+++ os-temporary/v2_common.sql
@@ -0,0 +1,6 @@
+alter table v2_user.module_menu rename to role_menu;
+alter table v2_user.menu_operate rename to role_operate;
+
+alter table v2_system.module rename to role_module;
+alter table v2_system.menu rename to role_menu;
+alter table v2_system.menu_operate rename to role_operate;
