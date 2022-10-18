4c4f07c49f5ec393ce14a17dfaaff9db12858262
linbaby <1065953740@qq.com>
2022-10-18 19:51:42 +0800

    2022-10-18 19:51
    1、表命名

--- os-temporary/v2_common.sql
+++ os-temporary/v2_common.sql
@@ -1,6 +1,6 @@
 alter table v2_user.module_menu rename to role_menu;
 alter table v2_user.menu_operate rename to role_operate;
 
-alter table v2_system.module rename to role_module;
-alter table v2_system.menu rename to role_menu;
-alter table v2_system.menu_operate rename to role_operate;
+alter table v2_system.role_module rename to module;
+alter table v2_system.role_menu rename to menu;
+alter table v2_system.role_operate rename to menu_operate;
